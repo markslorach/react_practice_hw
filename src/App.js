25de1b0ebfc92666c352e459cc7994913caad6c7
Mark Slorach <hello@markslorach.com>
2023-06-27 09:26:17 +0100

    react practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const name = 'Mark';
+  const age = 31;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a paragraph</p>
+      <p>My name is {name} and I'm {age} years old.</p>
     </div>
   );
 }
